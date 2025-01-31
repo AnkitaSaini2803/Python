0c123ddb4dde63c46f0f4ca579b56093baab07e1
AnkitaSaini2803 <ankitasainixib27@gmail.com>
2025-01-31 15:01:57 +0530

    Add

---
+++ Practice2.py
@@ -0,0 +1,76 @@
+# #1                                                 30/1/2025
+# i = 0
+# while i <= 7:    
+#     if i == 4:
+#         i += 1
+#         continue  
+#     print("" , i)
+#     i += 1
+
+# #
+
+# from typing import *
+# def compareIfElse(a: int, b: int):
+
+#     # Write your code here
+#     if (a>b):
+#         return "greater"
+#     elif (a<b):
+#         return "smaller"
+#     else:
+#         return "equal"
+# # num1=input("Enter first number:")
+# # num2=input("Enter second number:")
+# print(compareIfElse(5,9))
+
+
+# #
+# try:
+#     a=10
+#     b=0
+#     # print(c)
+# except NameError:
+#     print("NameError occured")
+#  #
+# def fun(n):
+#     n=9
+#     return n
+# n=19
+# n=fun(n)       #-->nothing will be printed,  #n=fun(n)  --> 9
+# print(n)   #19    
+
+
+# # 
+# text=input()
+# if text>='a' and text<='z':
+#     print('0')
+# elif text>='A' and text<='Z':
+#     print('1')
+# else:
+#     print('-1')
+
+
+# print(0.3+0.1==0.4) #false
+# print(3+1==4) #true
+
+# #
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+c=int(input("Enter the value of c"))
+if a>b and a>c:
+    print(a)
+elif b>a and b>c:
+    print(b)
+elif c>a and c>b:
+    print(c)
+
+
+
+
+
+
+
+
+
+
+
