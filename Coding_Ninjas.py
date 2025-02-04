ae8aca9044e44b91de5df2f7a188a857f84da43b
AnkitaSaini2803 <ankitasainixib27@gmail.com>
2025-02-04 15:23:31 +0530

    Add

--- Coding_Ninjas.py
+++ Coding_Ninjas.py
@@ -50,3 +50,45 @@ elif b>=a and b>c:
     print(b)
 elif c>a and c>b:
     print(c)
+
+'''3.'''
+from typing import *
+from math import pi
+
+def areaSwitchCase(ch: int, a: List[float]):
+   if ch==1:
+       return f"{pi*a[0]*a[0]:.5f}"
+   elif ch==2:
+        return f"{a[0]*a[1] :.5f}"
+print(areaSwitchCase(1,[7]))
+print(areaSwitchCase(2,[2,3]))
+
+'''4.                       4/2/2025
+Given a number N, print sum of all even numbers from 1 to N.
+'''
+# solution 1
+number=int(input("Enter"))
+sum_even=0
+for i in range(1,number+1):
+ if i%2==0:
+   sum_even+=i
+print(sum_even)
+
+#solution 2        
+number=int(input("Enter"))
+sum_even=0
+for i in range(2,number+1,2):
+   sum_even+=i
+print(sum_even)
+
+'''5. Write a program to find x to the power n (i.e., x^n). Take x and n from the
+user. You need to print the answer.Note: For this question, you can assume that 0 
+raised to the power of 0 is 1
+'''         
+x,n=map(int,input("Enter values of x and n:").split())
+if x<=8 and n<=9:
+ if x==1 and n==0:
+  print('1')
+ else:
+  print(x**n)
+
