84584877cc49bc4d5556f20c537fa448bf5cf525
AnkitaSaini2803 <ankitasainixib27@gmail.com>
2025-02-16 18:18:46 +0300

    Add New File

---
+++ Method_Overloading.py
@@ -0,0 +1,39 @@
+'''                                                             16-2-2025
+There is no Method overloading in python like other languages like C++. But it can 
+be achieved in two ways:
+1. Inefficient - Using Multiple functions with same name .But only last function
+will be considered and rest will not be considered (hidden)
+
+2. Efficient - Using multiple dispatch decorator 
+'''
+def mul(a,b):
+    return a*b
+
+def mul(a,b):
+    return a+b
+
+def mul(a,b,c):
+    return a*b*c
+
+# print(mul(5,6)) This will show an error
+# print(mul(5,6))
+print(mul(5,6,2))
+
+
+# 2. Using multiple dispatch decorator
+from multipledispatch import dispatch 
+@dispatch(int,int)
+def fun(a,b):
+    return a*b
+
+@dispatch(int,int)
+def fun(a,b):
+    return a+b
+    
+@dispatch(int,int,int)
+def fun(a,b,c):
+    return a*b*c
+
+print(fun(5,6))
+print(fun(5,6))
+print(fun(5,6,2))
