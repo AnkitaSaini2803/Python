9f6b530ca0c78e910e56ad501d268fe4792d5283
AnkitaSaini2803 <ankitasainixib27@gmail.com>
2025-02-19 15:07:19 +0300

    Add File

--- Method_Overloading.py
+++ Method_Overloading.py
@@ -6,16 +6,17 @@ will be considered and rest will not be considered (hidden)
 
 2. Efficient - Using multiple dispatch decorator 
 '''
-def mul(a,b):
-    return a*b
 
+def mul(a):
+    return a*a
+ 
 def mul(a,b):
     return a+b
 
 def mul(a,b,c):
     return a*b*c
 
-# print(mul(5,6)) This will show an error
+# print(mul(5,6)) #This will show an error
 # print(mul(5,6))
 print(mul(5,6,2))
 
